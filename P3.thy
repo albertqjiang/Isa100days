e1903a8a5ec5265f9c6398cf9c3fe716a8cf261b
Albert Jiang <albert594250@gmail.com>
2020-11-29 16:59:18 +0000

    formalised more proofs in Isabelle list exercises

--- P3.thy
+++ P3.thy
@@ -25,5 +25,85 @@ theorem "rev(replace x y zs) = replace x y (rev zs)"
    apply auto
   done
 
+lemma "replace x y (replace u v zs) = replace u v (replace x y zs)"
+  nitpick
+  oops
+
+lemma "replace y z (replace x y zs) = replace x z zs"
+  nitpick
+  oops
+
+fun del1 :: "'a \<Rightarrow> 'a list \<Rightarrow> 'a list" where
+"del1 x Nil = Nil" |
+"del1 x (y # xs) = (if x=y then xs else (y # xs))"
+
+fun delall :: "'a \<Rightarrow> 'a list \<Rightarrow> 'a list" where
+"delall x Nil = Nil" |
+"delall x (y # xs) = (if x=y then (delall x xs) else (y # (delall x xs)))"
+
+theorem "del1 x (delall x xs) = delall x xs"
+  apply (induct xs)
+   apply auto
+  done
+
+theorem "delall x (delall x xs) = delall x xs"
+  apply (induct xs)
+   apply auto
+  done
+
+theorem "delall x (del1 x xs) = delall x xs"
+  apply (induct xs)
+   apply auto
+  done
+
+lemma "del1 x (del1 y zs) = del1 y (del1 x zs)"
+  nitpick
+  oops
+
+lemma "delall x (del1 y zs) = del1 y (delall x zs)"
+  nitpick
+  oops
+
+theorem "delall x (delall y xs) = delall y (delall x xs)"
+  apply (induct xs)
+   apply auto
+  done
+
+lemma "del1 y (replace x y xs) = del1 x xs"
+  nitpick
+  oops
+
+lemma "delall y (replace x y xs) = delall x xs"
+  nitpick
+  oops
+
+theorem "replace x y (delall x xs) = delall x xs"
+  apply (induct xs)
+   apply auto
+  done
+
+lemma "replace x y (delall z xs) = delall z (replace x y xs)"
+  nitpick
+  oops
+
+lemma "rev(del1 x xs) = del1 x (rev xs)"
+  nitpick
+  oops
+
+lemma [simp]: "delall x (xs @ [x]) = delall x xs"
+  apply (induct xs)
+  apply auto
+  done
+
+lemma [simp]: "x \<noteq> a \<Longrightarrow> delall x xs @ [a] = delall x (xs @ [a])"
+  apply (induct xs)
+   apply auto
+  done
+
+theorem "rev(delall x xs) = delall x (rev xs)"
+  apply (induct xs)
+   apply auto
+  done
+
 end
 
